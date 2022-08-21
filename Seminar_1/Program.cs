13aa4fa15ecee423393be1694629068ac311fe67
Vladislav Tkachenko <Vladislav_tkachenko_93@mail.ru>
2022-08-21 21:27:39 +0300

    Save1

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,79 @@
+﻿// Console.WriteLine("ВВедите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int sqr = number* number;
+
+// Console.WriteLine(sqr);
+
+// Console.WriteLine("Введите число 1");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число 2");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// if(number1==number2*number2)
+// {
+//     Console.WriteLine("Число 1 является квадратом");
+// }
+// else
+// {
+//     Console.WriteLine("Число 1 не является квадратом");
+// }
+
+// Console.WriteLine("Введите число");
+// int numberDay = Convert.ToInt32(Console.ReadLine());
+
+// if(numberDay<1 || numberDay>7)
+// {
+//     Console.WriteLine("Ошибка");
+// }
+// if(numberDay==1)
+// {
+//     Console.WriteLine("Понедельник");
+// }
+// if(numberDay==2)
+// {
+//     Console.WriteLine("Вторник");
+// }
+// if(numberDay==3)
+// {
+//     Console.WriteLine("Среда");
+// }
+// if(numberDay==4)
+// {
+//     Console.WriteLine("Четверг");
+// }
+// if(numberDay==5)
+// {
+//     Console.WriteLine("Пятница");
+// }
+// if(numberDay==6)
+// {
+//     Console.WriteLine("СУббота");
+// }
+// if(numberDay==7)
+// {
+//     Console.WriteLine("Воскресенье");
+// }
+
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int numberNeg = number * (-1);
+
+// while(numberNeg <= number)
+// {
+//     Console.WriteLine(numberNeg);
+//     numberNeg++;
+// }
+
+Console.WriteLine("Введите число 1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1>number2)
+{
+    Console.WriteLine("Первое чило большее, второе меньшее");
+}
+if(number1<number2)
+{
+    Console.WriteLine("Первое чило меньшее, второе большее");
+}
