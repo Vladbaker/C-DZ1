f695405ef4f20ed1607b8bbbb0795f02a25d0588
Vladislav Tkachenko <Vladislav_tkachenko_93@mail.ru>
2022-08-28 15:26:16 +0300

    Save3 dz2

--- Seminar_2/Program.cs
+++ Seminar_2/Program.cs
@@ -1,44 +1,63 @@
-﻿// int num = new Random().Next(10, 100);
-// int num1 = num/10;
-// int num2 = num%10;
-// int max = num1;
+﻿int num = new Random().Next(10, 100);
+int num1 = num/10;
+int num2 = num%10;
+int max = num1;
 
-// if(max<num2)
-// {
-//     max=num2;
-// }
-// Console.WriteLine(num);
-// Console.WriteLine(max);
+if(max<num2)
+{
+    max=num2;
+}
+Console.WriteLine(num);
+Console.WriteLine(max);
 
 // Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа. 
 
-// int num = new Random().Next(100, 1000);
-// int num1 = num/100;
-// int num2 = num%10;
+int num = new Random().Next(100, 1000);
+int num1 = num/100;
+int num2 = num%10;
 
-// Console.WriteLine(num);
-// Console.Write(num1);
-// Console.Write(num2);
+Console.WriteLine(num);
+Console.Write(num1);
+Console.Write(num2);
 
 // Задача №10. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-// int num = new Random().Next(100, 1000);
-// Console.WriteLine(num);
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
 
-// Console.WriteLine("Введите трёхзначное число");
-// int num = Convert.ToInt32(Console.ReadLine());
-// int num1 = num / 10;
-// int num2 = num1 % 10;
+Console.WriteLine("Введите трёхзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = num / 10;
+int num2 = num1 % 10;
 
-// Console.WriteLine(num2);
+Console.WriteLine(num2);
 
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-// Console.WriteLine("Введите число");
-// int num = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine(num>99 ? num.ToString()[2] : "Цифра отсутсвует");
-//         Console.ReadKey(true);
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(num>99 ? num.ToString()[2] : "Цифра отсутсвует");
+        Console.ReadKey(true);
 // Честно признаюсь решение нашёл  в интернете, долго над ним думал но на 100% до конца так его и не понимаю,
-//недостаточно знаний по синтаксису, если можете обьясните пожалуйста и скажите откуда мне знать синтаксис если не на лекциях
+// недостаточно знаний по синтаксису, если можете обьясните пожалуйста и скажите откуда мне знать синтаксис если не на лекциях
 // не на семинарах его почти не обьясняют и не рассказывают.
 
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите цифру обозначающую день недели");
+int week = Convert.ToInt32(Console.ReadLine());
+if (week > 0 && week < 8)
+{
+    if (week == 6 || week == 7)
+    {
+    Console.WriteLine("Выходной");
+    }
+    else
+    {   
+    Console.WriteLine("Будний день");
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка");
+}
